11b0cdfad4fe2dbbd586a64c566b09a14b3534b0
bspy02 <bspy02@hku.hk>
2020-10-11 17:23:43 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;  // Import the Scanner class
+import java.util.Random;
+class Main 
+{
+    public static void main(String[] args) 
+    {
+        int n     = Integer.parseInt(input("Length: "));
+        int range = Integer.parseInt(input("Range: "));        
+        ArrayList<Integer> nums = randlist(n,range);
+        System.out.println(nums);
+        System.out.println(insertsort(nums));
+        System.out.println(nums);
+
+    }
+    public static ArrayList<Integer> randlist(int n,int range)
+    {
+        ArrayList al = new ArrayList(); 
+        Random rand = new Random();
+
+        for (int j = 0; j<n; j++)
+        {
+            al.add(rand.nextInt(range));
+        }
+        return al;
+    }
+    public static ArrayList<Integer> insertsort(ArrayList<Integer> al)
+    {
+        ArrayList<Integer> lis = new ArrayList<>(al);
+        lis.add(0,0);
+        for (int i = 1; i < lis.size(); i++)
+        {
+            int num = lis.get(i);
+            for (int j = i-1; j>=0; j--)
+            {
+                if(num>lis.get(j))
+                {
+                    lis.add(j+1,num);lis.remove(i+1);
+                    break;
+                }
+            }
+        }
+        lis.remove(0);
+        return lis;
+    }
+    public static void print(String s)
+    {
+        System.out.println(s);
+    }
+    public static String input(String question)
+    {
+        Scanner myObj = new Scanner(System.in); 
+        System.out.println(question);
+        String in = myObj.nextLine();  // Read user input
+        return in;  // Output user input
+    }
+}
